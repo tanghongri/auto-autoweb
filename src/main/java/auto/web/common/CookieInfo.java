bc8ae8850ad64917fbfe1cd9e66e6e75a9984bc3
tangh <tangh@DESKTOP-QF6UBRG>
2017-11-28 00:20:33 +0800

    cookies

---
+++ src/main/java/auto/web/common/CookieInfo.java
@@ -0,0 +1,13 @@
+package auto.web.common;
+
+import java.util.Date;
+
+public class CookieInfo {
+	public String name;
+	public String value;
+	public String path;
+	public String domain;
+	public Date expiry;
+	public boolean secure;
+	public boolean httpOnly;
+}
